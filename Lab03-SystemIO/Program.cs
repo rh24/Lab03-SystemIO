cc889972c689ec0a987e7903cb8799b413c149cd
rebeccahong <rebeccayhong@gmail.com>
2018-10-11 10:19:06 -0700

    Add Create file method

--- Lab03-SystemIO/Program.cs
+++ Lab03-SystemIO/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Lab03_SystemIO
 {
@@ -6,7 +7,22 @@ namespace Lab03_SystemIO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Create("words.txt");
+        }
+
+        static void Create(string path)
+        {
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.Create(path))
+                {
+                    sw.WriteLine = "moist";
+                    sw.WriteLine = "chocolate";
+                    sw.WriteLine = "turtles";
+                    sw.WriteLine = "easter";
+                    sw.WriteLine = "christmas";
+                }
+            }
         }
     }
 }
